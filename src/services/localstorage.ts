7ed1663d765fcc09aeb5ad8c060be77785df9d9e
Müller Esposito Nunes <mulleresposito@hotmail.com>
2022-04-08 01:59:39 -0300

    feat(localStorage): creates localStorage service;

---
+++ src/services/localstorage.ts
@@ -0,0 +1,15 @@
+interface Credential {
+    user: string;
+    password: string;
+}
+
+export const saveUser = (credential: Credential): void => {
+    const usersStorage = localStorage.getItem("@ELOCHALLENGE-CREDENTIALS");
+    const users: Credential[] = usersStorage ? JSON.parse(usersStorage) : [];
+    
+    const userAlreadyExist = users.some(({ user }) => user === credential.user);
+    if (userAlreadyExist) throw new Error("Usuário já existe no local storage!");
+
+    users.push(credential);
+    localStorage.setItem("@ELOCHALLENGE-CREDENTIALS", JSON.stringify(users));
+}
