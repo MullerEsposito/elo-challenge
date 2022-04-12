e2aa8293b7ba1ac3091e2424875af28214673c21
Müller Esposito Nunes <mulleresposito@hotmail.com>
2022-04-12 17:46:11 -0300

    feat(routes): creates the routes controller;

---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import { Routes as DOMRoutes, Route } from "react-router-dom"
+import App from "../App";
+import { Home } from "../pages/Home";
+import { LeadsPanel } from "../pages/LeadsPanel";
+import { RegisterLead } from "../pages/RegisterLead";
+import { RegisterUser } from "../pages/RegisterUser";
+
+export function Routes(): JSX.Element {
+  return (
+    <DOMRoutes>
+      <Route path="/" element={<Home />} /> 
+      <Route path="/users/register" element={<RegisterUser />} />
+      <Route path="/leads" element={<LeadsPanel />} />
+      <Route path="/leads/register" element={<RegisterLead />} />            
+      <Route path="*" element={<main><p>Página não encontrada!</p></main>} />
+    </DOMRoutes>
+  );
+};
