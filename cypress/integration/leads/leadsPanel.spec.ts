3612e21d5f8c67d87961921ab26b63f16d522eb4
Müller Esposito Nunes <mulleresposito@hotmail.com>
2022-04-12 21:28:27 -0300

    test(leadsPanel): creates tests to Lead Panel;

---
+++ cypress/integration/leads/leadsPanel.spec.ts
@@ -0,0 +1,28 @@
+describe("Leads Panel", () => {
+  it("tests whether all fields are on screen.", () => {
+    cy.visit("http://localhost:3000/leads");
+
+    cy.get("button").should("contain", "Novo Lead");
+    const table = cy.get("table");
+
+    table.get("tr").eq(0).get("th").eq(0).should("contain", "Cliente em Potencial");
+    table.get("tr").eq(0).get("th").eq(1).should("contain", "Dados Confirmados");
+    table.get("tr").eq(0).get("th").eq(2).should("contain", "Reunião Agendada");
+  });
+
+  it.skip("should be only possible drag a lead to a next column.", () => {
+    cy.visit("http://localhost:3000/leads/register");
+
+    cy.get("[name=name]").type("New Lead");
+    cy.get("[name=phone]").type("38984171252");
+    cy.get("[name=email]").type("newlead@hotmail.com");
+    cy.get("[type=checkbox]").check(["RPA", "Analytics"], { force: true });
+    
+    cy.contains("Salvar").click();
+    cy.visit("http://localhost:3000/leads");
+
+    cy.get("[#newlead@hotmail.com0]").dragTo();
+    const droppable = cy.get<HTMLElement>("[#newlead@hotmail.com]");
+
+  })
+});
