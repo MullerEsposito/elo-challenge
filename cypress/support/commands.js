e2aa1e00bc07534e8da4cf959a3b80adc727604e
Müller Esposito Nunes <mulleresposito@hotmail.com>
2022-04-13 03:01:05 -0300

    test(cypress): creates function to control the speed of events in Cypress;

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,15 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+const COMMAND_DELAY = 1000;
+const commands = ['click'];
+
+
+for (const command of commands) {
+  Cypress.Commands.overwrite(command, (originalFn, ...args) => {
+      const origVal = originalFn(...args);
+
+      return new Promise((resolve) => {
+          setTimeout(() => {
+              resolve(origVal);
+          }, COMMAND_DELAY);
+      });
+  });
+}; 
