5eb17befd57f45a87a628dd43ca369838afa15e0
Müller Esposito Nunes <mulleresposito@hotmail.com>
2022-04-14 20:45:25 -0300

    test(leadsPanel): finish tests from Leads Panel page;

--- cypress/integration/leads/leadsPanel.spec.ts
+++ cypress/integration/leads/leadsPanel.spec.ts
@@ -1,3 +1,6 @@
+import { getDroppableSelector } from "../util";
+import * as keyCodes from "../key-codes"
+
 describe("Leads Panel", () => {
   it("tests whether all fields are on screen.", () => {
     cy.visit("http://localhost:3000/leads");
@@ -10,7 +13,7 @@ describe("Leads Panel", () => {
     table.get("tr").eq(0).get("th").eq(2).should("contain", "Reunião Agendada");
   });
 
-  it.skip("should be only possible drag a lead to a next column.", () => {
+  it("should be able to drag a lead just to the column right after it.", () => {
     cy.visit("http://localhost:3000/leads/register");
 
     cy.get("[name=name]").type("New Lead");
@@ -21,8 +24,94 @@ describe("Leads Panel", () => {
     cy.contains("Salvar").click();
     cy.visit("http://localhost:3000/leads");
 
-    cy.get("[draggable-id=newlead@hotmail.com0]").move({ deltaX: 200, deltaY: 200 });
-    
+    cy.contains("New Lead").as("potential-status").should("have.attr", "data-rbd-draggable-id", "newlead@hotmail.com0");
 
-  })
+    cy.get("@potential-status")
+      .focus()
+      .trigger('keydown', { keyCode: keyCodes.space })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .wait(1000)
+      .trigger('keydown', { keyCode: keyCodes.space, force: true });
+
+    cy.contains("New Lead").should("not.have.attr", "data-rbd-draggable-id", "newlead@hotmail.com2");
+    cy.contains("New Lead").should("have.attr", "data-rbd-draggable-id", "newlead@hotmail.com0");
+
+    cy.get("@potential-status")
+      .focus()
+      .trigger('keydown', { keyCode: keyCodes.space })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .wait(1000)
+      .trigger('keydown', { keyCode: keyCodes.space, force: true });
+    
+    cy.contains("New Lead").should("not.have.attr", "data-rbd-draggable-id", "newlead@hotmail.com0");
+    cy.contains("New Lead").should("have.attr", "data-rbd-draggable-id", "newlead@hotmail.com1");
+
+    cy.contains("New Lead").as("confirmed-data").should("have.attr", "data-rbd-draggable-id", "newlead@hotmail.com1");
+    cy.get("@confirmed-data")
+      .focus()
+      .trigger('keydown', { keyCode: keyCodes.space })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .wait(1000)
+      .trigger('keydown', { keyCode: keyCodes.space, force: true });
+
+    cy.contains("New Lead").should("not.have.attr", "data-rbd-draggable-id", "newlead@hotmail.com1");
+    cy.contains("New Lead").should("have.attr", "data-rbd-draggable-id", "newlead@hotmail.com2");
+  });
+
+  it("shouldn't been able to drag to a column before.", () => {
+    cy.visit("http://localhost:3000/leads/register");
+
+    cy.get("[name=name]").type("New Lead");
+    cy.get("[name=phone]").type("38984171252");
+    cy.get("[name=email]").type("newlead@hotmail.com");
+    cy.get("[type=checkbox]").check(["RPA", "Analytics"], { force: true });
+    
+    cy.contains("Salvar").click();
+    cy.visit("http://localhost:3000/leads");
+
+    cy.contains("New Lead").as("potential-status").should("have.attr", "data-rbd-draggable-id", "newlead@hotmail.com0");
+
+    cy.get("@potential-status")
+      .focus()
+      .trigger('keydown', { keyCode: keyCodes.space })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .wait(1000)
+      .trigger('keydown', { keyCode: keyCodes.space, force: true });
+
+    cy.contains("New Lead").should("not.have.attr", "data-rbd-draggable-id", "newlead@hotmail.com0");
+    cy.contains("New Lead").should("have.attr", "data-rbd-draggable-id", "newlead@hotmail.com1");
+
+    cy.contains("New Lead").as("confirmed-data").should("have.attr", "data-rbd-draggable-id", "newlead@hotmail.com1");
+    cy.get("@confirmed-data")
+      .focus()
+      .trigger('keydown', { keyCode: keyCodes.space })
+      .trigger('keydown', { keyCode: keyCodes.arrowLeft, force: true })
+      .wait(1000)
+      .trigger('keydown', { keyCode: keyCodes.space, force: true });
+
+    cy.contains("New Lead").should("not.have.attr", "data-rbd-draggable-id", "newlead@hotmail.com0");
+    cy.contains("New Lead").should("have.attr", "data-rbd-draggable-id", "newlead@hotmail.com1");
+
+    cy.get("@confirmed-data")
+      .focus()
+      .trigger('keydown', { keyCode: keyCodes.space })
+      .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+      .wait(1000)
+      .trigger('keydown', { keyCode: keyCodes.space, force: true });
+
+    cy.contains("New Lead").should("not.have.attr", "data-rbd-draggable-id", "newlead@hotmail.com1");
+    cy.contains("New Lead").should("have.attr", "data-rbd-draggable-id", "newlead@hotmail.com2");
+
+    cy.contains("New Lead").as("schelled-meet").should("have.attr", "data-rbd-draggable-id", "newlead@hotmail.com2");
+    cy.get("@schelled-meet")
+      .focus()
+      .trigger('keydown', { keyCode: keyCodes.space })
+      .trigger('keydown', { keyCode: keyCodes.arrowLeft, force: true })
+      .wait(1000)
+      .trigger('keydown', { keyCode: keyCodes.space, force: true });
+
+    cy.contains("New Lead").should("not.have.attr", "data-rbd-draggable-id", "newlead@hotmail.com1");
+    cy.contains("New Lead").should("have.attr", "data-rbd-draggable-id", "newlead@hotmail.com2");
+  });
 });
